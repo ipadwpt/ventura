13630e98b06198619ed4ce34a688ae6c17298426
ipadwpt <s@goerke.net>
2013-10-30 02:23:48 +0100

    Grundrahmen aufgebaut

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Ventura | Start</title>
+<link href="css/reset.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="screen" />
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onload="MM_preloadImages('img/button-druck-m.png','img/button-facebook-m.png','img/button-mail-m.png')">
+<div id="container">
+<div id="header">
+<ul id="navigation">
+    <li><a href="kunde.php"><span>Kunde</span></a></li>
+    <li><a href="personal.php">Personal</a></li>
+    <li><a href="bewerber.php">Bewerber</a></li>
+    <li><a href="start.php">Start</a></li>
+</ul>
+</div> <!--header-->
+
+<div id="buehne">
+
+
+</div> <!--buehne-->
+
+<div id="footer">
+<div class="footertext" style="margin-left:42px;">Hotline <span>0202–26934850</span></div>
+<div class="footertext" style="margin-left:370px; margin-top:45px;"><a href="kontakt-anfahrt.php">Kontakt|Anfahrt</a></div>
+<div class="footertext" style="margin-left:530px; margin-top:45px;"><a href="abg.php">ABG</a></div>
+<div class="footertext" style="margin-left:600px; margin-top:45px;"><a href="impressum.php">Impressum</a></div>
+
+<div class="footertext" style="margin-left:800px; margin-top:40px;"><a href="mailto:info@ventura-personal.de" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('mail','','img/button-mail-m.png',1)"><img src="img/button-mail.png" alt="E-Mail" width="42" height="30" id="mail" /></a></div>
+
+<div class="footertext" style="margin-left:860px; margin-top:40px;"><a href="#" onclick="window.print();" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('druck','','img/button-druck-m.png',1)"><img src="img/button-druck.png" alt="Drucken" width="42" height="30" id="druck" /></a></div>
+
+<div class="footertext" style="margin-left:920px; margin-top:40px;"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('facebook','','img/button-facebook-m.png',1)"><img src="img/button-facebook.png" alt="Facebook" width="42" height="30" id="facebook" /></a></div>
+
+</div> <!--footer-->
+
+</div> <!--container-->
+</body>
+</html>
